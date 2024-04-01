44158511140691b8a33d47a8ce086b9b3dd3d1a0
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-04-01 15:57:22 -0300

    refactor: utilizando try-catch na requisição

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta); 
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
 }
