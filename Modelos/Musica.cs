fffc09fbf7125ddb273d585b709311b04ddf6f31
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-04-02 16:56:42 -0300

    feat: criação da pasta Modelos e da classe Música

---
+++ Modelos/Musica.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace ScreenSoundApi.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
+        Console.WriteLine($"Gênero: {Genero}");
+    }
+}
