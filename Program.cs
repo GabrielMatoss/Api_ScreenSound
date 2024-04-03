62b5c2a1642212fa7ebdd5b24639f77814c80153
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-04-02 22:10:32 -0300

    feat: utilizando o JsonSerializer desserializando em uma lista do tipo Musica

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using ScreenSoundApi.Modelos;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta); 
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
