3be2357d4889890cf244662d06b1cef540d9ff1a
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-04-04 09:28:45 -0300

    refactor: adicionando título descritivo antes de exibir a lista de generos musicais

--- Filtros/LinqFilter.cs
+++ Filtros/LinqFilter.cs
@@ -7,6 +7,7 @@ internal class LinqFilter
     public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
     {
         var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        Console.WriteLine("Lista de generos musicais \n");
         todosOsGenerosMusicais.ForEach(genero => Console.WriteLine($"- {genero}"));
     }
 }
